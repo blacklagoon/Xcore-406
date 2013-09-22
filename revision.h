33ec0d58f453bc6a79277182ef0007e56e6f54cd
castagne <kikate@hotmail.fr>
2013-09-22 11:55:31 +0200

    The first commit Ultracore G5 base

---
+++ revision.h
@@ -0,0 +1,11 @@
+#ifndef __REVISION_H__
+#define __REVISION_H__
+ #define _HASH                      "Archived"
+ #define _DATE                      "0000-00-00 00:00:00 +0000"
+ #define VER_COMPANYNAME_STR        "PDS-Ultracore & CCDB"
+ #define VER_LEGALCOPYRIGHT_STR     "(c)2010-2011 PDS-UC"
+ #define VER_FILEVERSION            4,0,6
+ #define VER_FILEVERSION_STR        "0000-00-00 00:00:00 +0000 (Archived)"
+ #define VER_PRODUCTVERSION         VER_FILEVERSION
+ #define VER_PRODUCTVERSION_STR     VER_FILEVERSION_STR
+#endif // __REVISION_H__
